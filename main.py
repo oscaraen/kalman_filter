36f62faf5cb871704fb06875198c7eb4d823eb99
Oscar Martínez <oscaraen@hotmail.com>
2022-10-03 19:16:13 -0500

    filtro funcional

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 # Script de ejemplo para el manejo de filtros de kalman (Kalman filter premier)
+import matplotlib
 import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
 import numpy as np
 
-muestras = 1000
-ruidosa = np.random.normal(0.8624, 0.2,muestras)
-Sm = np.mean(ruidosa)
-Sdv = np.std(ruidosa)
+n_samples = 1000
+samples = np.random.normal(0.8624, 0.04, n_samples)
+Sm = np.mean(samples)
+Sdv = np.std(samples)
 
 # Espacio de estados del modelo
-Q = 1e-5 # por experiencia se escoge la del proceso pequeña (pero mas grande que la real para que la abarque)
+Q = 1e-7 # por experiencia se escoge la del proceso pequeña (pero mas grande que la real para que la abarque)
 R = np.power(Sdv, 2) # varianza del sensor R, varianza del sensor al cuadrado
 
-X = np.zeros((1, muestras))
-P = np.zeros((1, muestras))
+X = np.zeros((n_samples))
+print(X.shape)
+P = np.zeros((n_samples))
 # esperar a la clase de Kalman para ver cómo se llena esto, subir el repo a Github para compartir con el portatil
 
 Xposterior = 0.8  # asumir un valor inicial de las cosas, le digo en donde empiece, o el primer dato del sensor.
 Pposterior = 1
 
-for i in range(len(ruidosa)):
+for i in range(len(samples)):
 
-    ym = ruidosa[i] # simular que acaba de llegar una medida
+    ym = samples[i]  # simular que acaba de llegar una medida
     # predict
-    Xprior = Xposterior # mejor primer prior es el posterior
-    Pprior = Pposterior + Q # asumiendo F = 1
+    Xprior = Xposterior  # mejor primer prior es el posterior
+    Pprior = Pposterior + Q  # asumiendo F = 1
 
     # update
-    K = Pprior * 1/(Pprior + R) # H vale 1
+    K = Pprior * 1/(Pprior + R)  # H vale 1
     Xposterior = Xprior + K * (ym - Xprior)
     Pposterior = (1 - K) * Pprior
 
     # store result
-    np.append(X, Xposterior)
-    np.append(P, Pposterior)
+    X[i] = Xposterior
+    P[i] = Pposterior
 
-plt.plot(muestras)
-plt.plot(X) #
-plt.plot(np.ones(muestras)*Sm) # ground truth
+print(len(X))
+plt.scatter(range(n_samples),samples)
+plt.plot(X, color="green") # filtro de kalman en azul
+plt.plot(np.ones(n_samples)*Sm, color="red") # ground truth
 
 plt.show()
